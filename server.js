18e430336988886a539d09cf573448bd0f52796d
Ranjith <Ranjiththangavel11@gmail.com>
2024-04-02 22:19:47 +0530

    Last update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
 const Files = require("./models/File")
 
-const upload = multer({dest : "uploads"})
+// const upload = multer({dest : "uploads"})
 
 const app = express()
 app.use(express.urlencoded({ extended: true}))
@@ -18,7 +18,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect(process.env.DATABASE_URL)
 
-
+count = 1
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null,count++ + "-"+ file.originalname)
+    }
+  })
+  const upload = multer({ storage: storage })
 
 app.get("/",(req, res) => {
     res.render("index")
@@ -31,8 +41,6 @@ app.post("/upload", upload.single("file"), async (req, res) => {
         orginalname: req.file.originalname
     }
     console.log(req.file);
-
-
     const file = await Files.create(fileData)
     res.render("index", { fileLink: `${process.env.APP_BASE_URL}/${file.id}` })
     
