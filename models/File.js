25a404857a6e204d8702532628cc54ceec8c9ca1
Ranjith <Ranjiththangavel11@gmail.com>
2024-04-03 21:50:51 +0530

    Succefuly Running

--- models/File.js
+++ models/File.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose");
+import { Schema, model } from "mongoose";
 
-const fileSchema = new mongoose.Schema({
+const fileSchema = new Schema({
     path: {
         type: String,
         required: true
@@ -16,4 +16,4 @@ const fileSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model("File", fileSchema);
+export default model("File", fileSchema);
